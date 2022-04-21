928e729dba93a4231b7362613f2b23cbee88b349
Jorge Silva <jorge@edgeimpulse.com>
2022-04-21 18:50:16 +0000

    Release 21 April 2022

--- spectrogram/dsp.py
+++ spectrogram/dsp.py
@@ -11,7 +11,7 @@ from scipy import signal as sn
 import math
 
 # Load our SpeechPy fork
-MODULE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'third_party', 'speechpy', '__init__.py')
+MODULE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'third_party', 'speechpy', '__init__.py')
 MODULE_NAME = 'speechpy'
 import importlib
 import sys
